e2a02c29ed83ff37a5d9866e7c38fffbe338ff58
Evandro Sacramento <evandro-dev@outlook.com>
2025-07-18 13:13:38 -0300

    first commit

---
+++ src/main/java/com/essj/clinix/ClinixApplication.java
@@ -0,0 +1,13 @@
+package com.essj.clinix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinixApplication.class, args);
+	}
+
+}
