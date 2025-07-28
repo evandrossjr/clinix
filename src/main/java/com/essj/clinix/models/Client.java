035520282230b50bf8168705064965ff7676a115
Evandro Sacramento II <evandro-dev@outlook.com>
2025-07-27 22:38:35 -0300

    Class Client.java

--- src/main/java/com/essj/clinix/models/Client.java
+++ src/main/java/com/essj/clinix/models/Client.java
@@ -1,4 +1,17 @@
 package com.essj.clinix.models;
 
-public class Client {
+import com.essj.clinix.models.enums.Role;
+
+import java.util.Set;
+
+public class Client extends User{
+
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String username, String password, String cpf, String email, String phoneNumber, Set<Role> roles) {
+        super(id, name, username, password, cpf, email, phoneNumber, roles);
+    }
+
 }
